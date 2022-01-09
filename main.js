395bb196b80ea3356871a5446c9d5dea31bda91e
susmithamerugu <81218368+susmithamerugu@users.noreply.github.com>
2022-01-09 14:18:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function play_song1(){
+     document.getElementById("audio1").play();
+}
+function pause_song1(){
+     document.getElementById("audio1").pause();
+}
+function play_song2(){
+     document.getElementById("audio2").play();
+}
+function pause_song2(){
+     document.getElementById("audio2").pause();
+}
